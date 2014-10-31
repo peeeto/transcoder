6296bca2d90fe320f77542b272850d2b8e5c1c70
peeeto <peeeto@seznam.cz>
2014-10-31 13:45:12 +0200

    added absolute path

--- transcoder.sh
+++ transcoder.sh
@@ -1,4 +1,4 @@
 
 #sudo ln -s $(pwd)/transcoder.sh /usr/bin/transcoder
-java -jar target/transcoder-1.2-SNAPSHOT.one-jar.jar 
+java -jar /Users/petercho/devel/transcoder/target/transcoder-1.2-SNAPSHOT.one-jar.jar 
 
