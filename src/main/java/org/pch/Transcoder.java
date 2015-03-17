806b208ec30e906a6cd4f689b23e0907ba3b2ee9
peeeto <peeeto@seznam.cz>
2015-03-17 18:32:53 +0200

    support for bcrypt added

--- src/main/java/org/pch/Transcoder.java
+++ src/main/java/org/pch/Transcoder.java
@@ -7,27 +7,32 @@ import javax.swing.*;
 
 public class Transcoder {
 
-  public static void main(String args[]) {
-    final AtomicReference<Component> componentRef = new AtomicReference<Component>();
-    SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
-        JDialog tf = new TranscoderFrame();
-        componentRef.set(tf);
-        tf.pack();
-        tf.setVisible(true);
-      }
-    });
-    Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
-                                                @Override
-                                                public void uncaughtException(Thread t, Throwable e) {
-                                                  e.printStackTrace();
-                                                  JOptionPane.showMessageDialog(componentRef.get(),
-                                                      e + "",
-                                                      "Unexpected Error",
-                                                      JOptionPane.ERROR_MESSAGE
-                                                  );
-                                                }
-                                              }
-    );
-  }
+    private static final AtomicReference<Component> componentRef = new AtomicReference<Component>();
+
+    public static Component getInstance() {
+        return componentRef.get();
+    }
+
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                TranscoderFrame tf = new TranscoderFrame();
+                componentRef.set(tf);
+                tf.pack();
+                tf.setVisible(true);
+            }
+        });
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(componentRef.get(),
+                        e + "",
+                        "Unexpected Error",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            }
+        }
+        );
+    }
 }
